b9b4611fae112993b80e4393b83f1a2814f1f2d7
YasasDaraka <darakayasas@gmail.com>
2024-01-29 10:52:20 +0530

    BackEnd update to JNDI

--- BackEnd/src/main/java/lk/ijse/pos/listener/ContextListener.java
+++ BackEnd/src/main/java/lk/ijse/pos/listener/ContextListener.java
@@ -2,22 +2,24 @@ package lk.ijse.pos.listener;
 
 import org.apache.commons.dbcp2.BasicDataSource;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+import javax.sql.DataSource;
 
 @WebListener
 public class ContextListener implements ServletContextListener {
-    public static BasicDataSource pool;
+    public static DataSource pool;
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        pool = new BasicDataSource();
-        pool.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        pool.setUrl("jdbc:mysql://localhost:3306/Javaee");
-        pool.setUsername("root");
-        pool.setPassword("1234");
-        pool.setInitialSize(10);
-        pool.setMaxTotal(20);
+        try {
+            InitialContext context = new InitialContext();
+            pool = (DataSource) context.lookup("java:comp/env/bean/MyBeanFactory");
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
