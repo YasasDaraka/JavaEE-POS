92112888503d82c5ccf4ac8564b3246d79891fbd
YasasDaraka <darakayasas@gmail.com>
2024-01-30 12:18:26 +0530

    Update

--- BackEnd/src/main/java/lk/ijse/pos/filter/CORSFilter.java
+++ BackEnd/src/main/java/lk/ijse/pos/filter/CORSFilter.java
@@ -14,7 +14,7 @@ public class CORSFilter extends HttpFilter {
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
         System.out.println("Core filter Response in");
         res.addHeader("Access-Control-Allow-Origin","*");
-        res.addHeader("Access-Control-Allow-Methods","GET,POST,PUT,DELETE,OPTION");
+        res.addHeader("Access-Control-Allow-Methods","GET,POST,PUT,DELETE,OPTIONS");
         res.addHeader("Access-Control-Allow-Headers","Content-Type");
         chain.doFilter(req, res);
         System.out.println("Core filter Response out");
